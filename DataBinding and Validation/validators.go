d1a7ba1282680c84324903c2dbab2b26fe5f37f0
Gopikanth <89236046+Gopikanth@users.noreply.github.com>
2021-11-22 18:45:59 +0530

    Update validators.go

--- DataBinding and Validation/validators.go
+++ DataBinding and Validation/validators.go
@@ -6,7 +6,7 @@ import (
 	"gopkg.in/go-playground/validator.v9" //this package is imported for the validation
 )
 
-// ValidateCoolTitle returns true when the field value contains the word "cool".
+// ValidateCoolTitle returns true when the field value contains the word "celebrity".
 func ValidateCoolTitle(fl validator.FieldLevel) bool {
 	return strings.Contains(fl.Field().String(), "celebrity")
 }
